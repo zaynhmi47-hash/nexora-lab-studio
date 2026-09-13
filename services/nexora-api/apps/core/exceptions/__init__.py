bf036f4ce93851f9f9bfd13db92f34b9cf0fcf09
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-13 17:11:43 +0700

    fix(core): restore exception exports

---
+++ services/nexora-api/apps/core/exceptions/__init__.py
@@ -0,0 +1,25 @@
+from apps.core.exceptions.base import (
+    AuthenticationException,
+    ConflictException,
+    ConfigurationException,
+    DomainException,
+    ExternalServiceException,
+    NexoraException,
+    NotFoundException,
+    PermissionDeniedException,
+    RateLimitException,
+    ValidationException,
+)
+
+__all__ = [
+    "AuthenticationException",
+    "ConflictException",
+    "ConfigurationException",
+    "DomainException",
+    "ExternalServiceException",
+    "NexoraException",
+    "NotFoundException",
+    "PermissionDeniedException",
+    "RateLimitException",
+    "ValidationException",
+]
