15982ce962a7c38c71ab099a57b39cfd81b52524
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-13 16:53:35 +0700

    migrate capabilities model exports

---
+++ services/nexora-api/apps/capabilities/models/__init__.py
@@ -0,0 +1,4 @@
+from apps.capabilities.models.capability import Capability
+from apps.capabilities.models.product_capability import ProductCapability
+
+__all__ = ["Capability", "ProductCapability"]
