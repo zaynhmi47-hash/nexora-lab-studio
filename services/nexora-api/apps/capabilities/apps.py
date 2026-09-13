a9f8893e80aeccb11be8634092aacb8fb88b72f6
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-13 16:53:17 +0700

    migrate capabilities app config

---
+++ services/nexora-api/apps/capabilities/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CapabilitiesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.capabilities"
+    label = "capabilities"
