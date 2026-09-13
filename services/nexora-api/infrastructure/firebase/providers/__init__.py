771c0ec99d483a085cb5229481f24b89641b2aac
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-13 16:56:35 +0700

    migrate firebase providers init

---
+++ services/nexora-api/infrastructure/firebase/providers/__init__.py
@@ -0,0 +1,6 @@
+from infrastructure.firebase.providers.identity import FirebaseIdentityProvider
+from infrastructure.firebase.providers.messaging import FirebaseMessagingProvider
+from infrastructure.firebase.providers.realtime import FirebaseRealtimeDataProvider
+from infrastructure.firebase.providers.storage import FirebaseObjectStorageProvider
+
+__all__ = ["FirebaseIdentityProvider", "FirebaseMessagingProvider", "FirebaseObjectStorageProvider", "FirebaseRealtimeDataProvider"]
