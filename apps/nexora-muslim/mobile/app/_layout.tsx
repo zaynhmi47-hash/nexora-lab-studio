ebdd7ef9b04fbfd287024c8ac4a6e9ba402b2546
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-18 22:10:21 +0700

    feat(places): register mobile Islamic places route

--- apps/nexora-muslim/mobile/app/_layout.tsx
+++ apps/nexora-muslim/mobile/app/_layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout() {
               <Tabs.Screen name="calendar" options={{ href: null }} />
               <Tabs.Screen name="zakat" options={{ href: null }} />
               <Tabs.Screen name="worship" options={{ href: null }} />
-              <Tabs.Screen name="reminders" options={{ href: null }} />
+              <Tabs.Screen name="reminders" options={{ href: null }} />\n              <Tabs.Screen name="places" options={{ href: null }} />
             </Tabs>
           </AppStateProvider>
         </AuthGate>
