a0257a930ff03c687c2879c3e32809708fd40703
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-19 00:58:22 +0700

    feat(finance): add adaptive application shell

--- apps/nexora-finance/mobile/components/layout/index.ts
+++ apps/nexora-finance/mobile/components/layout/index.ts
@@ -1,2 +1,5 @@
+export * from './AdaptiveHeader';
+export * from './AdaptiveNavigation';
 export * from './ResponsiveContainer';
 export * from './ResponsiveGrid';
+export * from './ResponsiveScaffold';
