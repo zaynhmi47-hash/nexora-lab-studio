9f2ce2f1a8a8636238c48647874654c6b90d6fb3
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-18 11:17:44 +0700

    feat(tajwid): add api routes

---
+++ services/nexora-api/apps/tajwid/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .api import (
+    TajwidAssessmentView,
+    TajwidCompleteTopicView,
+    TajwidPracticeView,
+    TajwidProgressView,
+    TajwidTopicsView,
+)
+
+urlpatterns = [
+    path("topics/", TajwidTopicsView.as_view()),
+    path("progress/", TajwidProgressView.as_view()),
+    path("topics/<slug:topic_key>/practice/", TajwidPracticeView.as_view()),
+    path("topics/<slug:topic_key>/complete/", TajwidCompleteTopicView.as_view()),
+    path("assessment/complete/", TajwidAssessmentView.as_view()),
+]
