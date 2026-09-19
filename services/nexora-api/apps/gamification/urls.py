51a20e4a704674595733bf8e21dd80ce42caceb6
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-19 18:12:24 +0700

    feat(gamification): expose daily reward route

--- services/nexora-api/apps/gamification/urls.py
+++ services/nexora-api/apps/gamification/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .api import GamificationActivityView, GamificationStatisticsView
+from .api import GamificationActivityView, GamificationStatisticsView, GamificationDailyRewardView
 
 urlpatterns = [
     path("activities/", GamificationActivityView.as_view(), name="gamification-activities"),
     path("statistics/", GamificationStatisticsView.as_view(), name="gamification-statistics"),
+    path("daily-reward/", GamificationDailyRewardView.as_view(), name="gamification-daily-reward"),
 ]
