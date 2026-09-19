f8f2e0fef71c225f37bdb6c43d02b2adceb4ee48
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-19 17:19:06 +0700

    feat(knowledge): add hadith favorites

--- services/nexora-api/apps/knowledge/urls.py
+++ services/nexora-api/apps/knowledge/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
-from .api import KnowledgeHadithView, KnowledgeTopicView, KnowledgeView
+from .api import HadithFavoriteListView, HadithFavoriteToggleView, KnowledgeHadithView, KnowledgeTopicView, KnowledgeView
 
-urlpatterns = [path("", KnowledgeView.as_view()), path("topics/<slug:key>/", KnowledgeTopicView.as_view()), path("hadith/<slug:key>/", KnowledgeHadithView.as_view())]
+urlpatterns = [
+    path("", KnowledgeView.as_view()),
+    path("topics/<slug:key>/", KnowledgeTopicView.as_view()),
+    path("hadith/<slug:key>/", KnowledgeHadithView.as_view()),
+    path("hadith-favorites/", HadithFavoriteListView.as_view()),
+    path("hadith-favorites/<uuid:hadith_id>/toggle/", HadithFavoriteToggleView.as_view()),
+]
