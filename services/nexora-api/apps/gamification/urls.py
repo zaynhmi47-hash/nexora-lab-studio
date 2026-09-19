9561e9568d1c05cd82d994a2cd6338b3c9411b7a
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-19 17:47:21 +0700

    feat(gamification): add statistics route

--- services/nexora-api/apps/gamification/urls.py
+++ services/nexora-api/apps/gamification/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .api import GamificationActivityView
+from .api import GamificationActivityView, GamificationStatisticsView
 
 urlpatterns = [
     path("activities/", GamificationActivityView.as_view(), name="gamification-activities"),
+    path("statistics/", GamificationStatisticsView.as_view(), name="gamification-statistics"),
 ]
