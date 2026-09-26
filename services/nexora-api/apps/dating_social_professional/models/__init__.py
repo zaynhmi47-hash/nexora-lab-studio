4cb1342755ae8c266ef2d44e575663849d06ec5e
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-26 10:35:41 +0800

    feat(dating): export push token model

--- services/nexora-api/apps/dating_social_professional/models/__init__.py
+++ services/nexora-api/apps/dating_social_professional/models/__init__.py
@@ -1,8 +1,9 @@
 from .conversation import DatingConversation, DatingMessage
 from .match import DatingMatch
 from .notification import DatingNotification
+from .push import DatingPushToken
 from .profile import DatingProfile
 from .safety import DatingBlock, DatingReport
 from .swipe import DatingSwipe
 
-__all__ = ["DatingBlock", "DatingConversation", "DatingMatch", "DatingMessage", "DatingNotification", "DatingProfile", "DatingReport", "DatingSwipe"]
+__all__ = ["DatingBlock", "DatingConversation", "DatingMatch", "DatingMessage", "DatingNotification", "DatingProfile", "DatingPushToken", "DatingReport", "DatingSwipe"]
