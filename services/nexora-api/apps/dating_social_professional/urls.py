90b4d3bb04f5b3c7f23a33a9b92839211c16c2bd
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-26 10:11:51 +0800

    feat(dating): route block endpoint

--- services/nexora-api/apps/dating_social_professional/urls.py
+++ services/nexora-api/apps/dating_social_professional/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .api import DiscoveryView, MatchesView, MeProfileView, SwipeView
+from .api import BlockView, DiscoveryView, MatchesView, MeProfileView, SwipeView
 
 urlpatterns = [
     path("discovery/", DiscoveryView.as_view(), name="dating-discovery"),
     path("profile/me/", MeProfileView.as_view(), name="dating-profile-me"),
     path("swipes/", SwipeView.as_view(), name="dating-swipe"),
     path("matches/", MatchesView.as_view(), name="dating-matches"),
+    path("blocks/", BlockView.as_view(), name="dating-block"),
 ]
