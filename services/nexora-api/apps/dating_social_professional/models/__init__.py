181b4a76bf3de0e224411a735459bb52174d75a6
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-26 10:11:24 +0800

    feat(dating): expose safety models

--- services/nexora-api/apps/dating_social_professional/models/__init__.py
+++ services/nexora-api/apps/dating_social_professional/models/__init__.py
@@ -1,5 +1,6 @@
 from .match import DatingMatch
 from .profile import DatingProfile
+from .safety import DatingBlock, DatingReport
 from .swipe import DatingSwipe
 
-__all__ = ["DatingMatch", "DatingProfile", "DatingSwipe"]
+__all__ = ["DatingBlock", "DatingMatch", "DatingProfile", "DatingReport", "DatingSwipe"]
