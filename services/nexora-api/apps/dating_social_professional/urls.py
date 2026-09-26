44ade372ff4ac14cb85b11416a6585ea1c485866
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-26 10:22:45 +0800

    feat(dating): expose messaging routes

--- services/nexora-api/apps/dating_social_professional/urls.py
+++ services/nexora-api/apps/dating_social_professional/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .api import BlockView, ConversationView, DiscoveryView, MatchesView, MeProfileView, ReportView, SwipeView
+from .api import BlockView, ConversationMessagesView, ConversationReadView, ConversationView, DiscoveryView, MatchesView, MeProfileView, ReportView, SwipeView
 
 urlpatterns = [
     path("discovery/", DiscoveryView.as_view(), name="dating-discovery"),
@@ -8,6 +8,8 @@ urlpatterns = [
     path("swipes/", SwipeView.as_view(), name="dating-swipe"),
     path("matches/", MatchesView.as_view(), name="dating-matches"),
     path("conversations/", ConversationView.as_view(), name="dating-conversation"),
+    path("conversations/<uuid:conversation_id>/messages/", ConversationMessagesView.as_view(), name="dating-conversation-messages"),
+    path("conversations/<uuid:conversation_id>/read/", ConversationReadView.as_view(), name="dating-conversation-read"),
     path("blocks/", BlockView.as_view(), name="dating-block"),
     path("reports/", ReportView.as_view(), name="dating-report"),
 ]
