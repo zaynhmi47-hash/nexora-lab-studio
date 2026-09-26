d5600c7bf2f22b1ea740a3e0ea2744d66c88b938
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-26 10:43:23 +0800

    feat(dating): expose preference and presence models

--- services/nexora-api/apps/dating_social_professional/models/__init__.py
+++ services/nexora-api/apps/dating_social_professional/models/__init__.py
@@ -3,7 +3,8 @@ from .match import DatingMatch
 from .notification import DatingNotification
 from .push import DatingPushToken
 from .profile import DatingProfile
+from .preferences import DatingConversationPresence, DatingNotificationPreference
 from .safety import DatingBlock, DatingReport
 from .swipe import DatingSwipe
 
-__all__ = ["DatingBlock", "DatingConversation", "DatingMatch", "DatingMessage", "DatingNotification", "DatingProfile", "DatingPushToken", "DatingReport", "DatingSwipe"]
+__all__ = ["DatingBlock", "DatingConversation", "DatingConversationPresence", "DatingMatch", "DatingMessage", "DatingNotification", "DatingNotificationPreference", "DatingProfile", "DatingPushToken", "DatingReport", "DatingSwipe"]
