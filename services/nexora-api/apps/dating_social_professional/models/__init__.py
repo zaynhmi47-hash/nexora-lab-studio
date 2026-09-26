ad2367bd8bd17f4939952f599314d4e3689eaac2
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-26 10:30:54 +0800

    feat(dating): export notification model

--- services/nexora-api/apps/dating_social_professional/models/__init__.py
+++ services/nexora-api/apps/dating_social_professional/models/__init__.py
@@ -1,7 +1,8 @@
 from .conversation import DatingConversation, DatingMessage
 from .match import DatingMatch
+from .notification import DatingNotification
 from .profile import DatingProfile
 from .safety import DatingBlock, DatingReport
 from .swipe import DatingSwipe
 
-__all__ = ["DatingBlock", "DatingConversation", "DatingMatch", "DatingMessage", "DatingProfile", "DatingReport", "DatingSwipe"]
+__all__ = ["DatingBlock", "DatingConversation", "DatingMatch", "DatingMessage", "DatingNotification", "DatingProfile", "DatingReport", "DatingSwipe"]
