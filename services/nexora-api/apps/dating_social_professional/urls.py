ba7698de8fccec365fa33aad7fe3c621fb23d686
zaynhmi47-hash <zayn.hmi47@gmail.com>
2026-09-26 10:10:47 +0800

    feat(dating): expand profile and match workflows

--- services/nexora-api/apps/dating_social_professional/urls.py
+++ services/nexora-api/apps/dating_social_professional/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .api import DiscoveryView, SwipeView
+from .api import DiscoveryView, MatchesView, MeProfileView, SwipeView
 
 urlpatterns = [
     path("discovery/", DiscoveryView.as_view(), name="dating-discovery"),
+    path("profile/me/", MeProfileView.as_view(), name="dating-profile-me"),
     path("swipes/", SwipeView.as_view(), name="dating-swipe"),
+    path("matches/", MatchesView.as_view(), name="dating-matches"),
 ]
